a3c7766da66bd0a6e8a48352b98c076f3a1e5af2
Cluster-YW <linziqwq@qq.com>
2026-01-15 16:18:11 +0800

    测试蜂鸣器

--- main.v
+++ main.v
@@ -1,16 +1,16 @@
 module main(
     input clk_1hz,       // 1Hz 秒时钟 (控制总时长)
     input clk_1khz,      // 1000Hz 音频时钟 (控制嘀嘀嘀的节奏)
-    input button_1,     // Pulse 
-    input button_2,     // QD
-    input button_3_raw,     // CLR(需要翻转)
-    input switch_clr,      // 复位信号
-    input switch_setting,  // 校时设定开关
-    input switch_alarm, // 闹钟开关
-    input switch_stopwatch, // 秒表开关
-    input switch_debug1, // 调试开关1
-    input switch_debug2, // 调试开关2
-    input switch_debug3, // 调试开关3
+    input btn_1,     // Pulse 
+    input btn_2,     // QD
+    input btn_3_raw,     // CLR(需要翻转)
+    input simu_hopper_stop, // 漏斗停止信号
+    input simu_hopper_add,  // 漏斗手动增加
+    input simu_conveyor_stop, // 传送带停止信号
+    input debug_1,
+    input debug_2,
+    input debug_3,
+    input debug_4,
     output [6:0] LED7S_out,
     output [3:0] LED7S2_out,
     output [3:0] LED7S3_out,
@@ -19,6 +19,30 @@ module main(
     output [3:0] LED7S6_out,
     output beep
 );
+
+    assign btn_3 = ~btn_3_raw;
+    assign hopper_signal = simu_hopper_stop? 1'b0 : clk_1hz;
+
+    // ==========================================
+    // 分频
+    // ==========================================
+    reg [9:0] cnt1k;
+    reg clk_4hz;
+    reg clk_2hz;
+
+    always @(posedge clk_1khz) begin
+        if cnt1k == 1000-1:
+            cnt1k <= 0;
+        else
+            cnt1k <= cnt1k + 1;
+        
+        if (cnt1k == 0 or cnt1k == 500)
+            clk_4hz <= ~clk_4hz;
+        
+        if (cnt1k == 0 or cnt1k == 250 or cnt1k == 500 or cnt1k == 750)
+            clk_2hz <= ~clk_2hz;
+    end
+    
 
     // ==========================================
     // 显示译码
@@ -35,12 +59,12 @@ module main(
     wire [3:0] display_6;
 
     // 调试显示
-    assign display_1 = 
-    assign display_2 = 
-    assign display_3 = 
-    assign display_4 = 
-    assign display_5 = 
-    assign display_6 = 
+    assign display_1 = 4'h1;
+    assign display_2 = 4'h2;
+    assign display_3 = 4'h3;
+    assign display_4 = 4'h4;
+    assign display_5 = 4'h5;
+    assign display_6 = 4'h6;
 
     reg [0:5] flicker_mask;
 
@@ -65,17 +89,8 @@ module main(
     // ==========================================
     // 蜂鸣器部分
     // ==========================================
-
     reg [4:0] beep_timer;
-    assign beep_enable = 1'b0;
-    assign beep = beep_enable && rhythm && clk_1khz;
 
-    always @(posedge clk_1hz) begin
-        if (beep_timer > 0) beep_timer <= beep_timer - 1;
-
-        if (en_clock_min_l)
-            beep_timer <= 4'd5;
-        
-    end
+    assign beep = ((debug_1) | (debug2 & clk_2hz) | (debug3 & clk_4hz)) & clk_1khz;
 
 endmodule
